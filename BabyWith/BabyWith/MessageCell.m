0bf44d1f760d74fb7841247969be889b1d3de647
XXF <feng.zhi.shang@qq.com>
2014-08-07 17:40:46 +0800

    ss
    sss

--- BabyWith/BabyWith/MessageCell.m
+++ BabyWith/BabyWith/MessageCell.m
@@ -16,7 +16,7 @@
     if (self) {
         // Initialization code
         
-        _alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 320, 40)];
+        _alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 320, 55)];
         _alertLabel.backgroundColor = [UIColor clearColor];
         _alertLabel.font = [UIFont systemFontOfSize:15.0];
         _alertLabel.lineBreakMode = NSLineBreakByWordWrapping;
